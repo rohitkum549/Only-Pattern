d02b7d85f3324c45d57665cea1eeff56aa32e383
Rohit Kumar Jha <57566698+rohitkum549@users.noreply.github.com>
2022-04-20 12:27:42 +0530

    Square
    O(n)^2

---
+++ square.java
@@ -0,0 +1,16 @@
+package code;
+
+public class square 
+{
+	public static void main (String args[])
+	{
+		for(int i=0;i<8;i++)
+		{
+			for(int j=0;j<8;j++) 
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+}
