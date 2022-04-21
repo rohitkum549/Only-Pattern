9ca08848a77c236cb9a589569027af2d49cf8853
Rohit Kumar Jha <57566698+rohitkum549@users.noreply.github.com>
2022-04-21 18:22:23 +0530

    Pyramid

---
+++ pyramid.java
@@ -0,0 +1,35 @@
+package code;
+
+import java.util.Scanner;
+
+public class pyramid 
+{
+	public int tree(int a)
+	{
+		int i;
+		for(i=0;i<a;i++)
+		{
+			for(int j=a;j>=1;j--)
+			{
+				if (j>i)
+				{
+					System.out.print(" ");
+				}
+				else
+				{
+					System.out.print(" *");
+				}
+			}
+			System.out.println();
+		}
+		return i;
+	}
+	public static void main (String args[])
+	{
+		System.out.print("Enter the number:-");
+		Scanner x=new Scanner(System.in);
+		int n=x.nextInt();
+		pyramid p=new pyramid(); //i created a object name p
+		int d=p.tree(n);// here i send the value of n 
+	}
+}
